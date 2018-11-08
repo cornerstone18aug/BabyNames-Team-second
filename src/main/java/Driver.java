15eb8ebf4b97578db9b729b830e0c92de6ba6224
nator333 <m.nakamata35@gmail.com>
2018-11-07 16:44:48 -0800

    Update Driver

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,9 +1,34 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Map;
+
 /**
  * Driver
  */
 public class Driver {
+  private Map<Integer, List<BabyName>>
 
   public static void main(String[] args) {
+
+    File[] htmls = new File("src/main/resources/").listFiles();
+    File outputFile = new File("src/main/resources/output.txt");
+
+    for(File html : htmls) {
+      try {
+        BufferedReader br = new BufferedReader(new FileReader(html))
+      }catch (IOException ioE) {
+
+      }
+
+
+
+
+    }
+
+
 
     // 1. Load HTMLs one by one
     //   1. Check file path and load
